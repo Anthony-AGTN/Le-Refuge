08ed2d7545f8c1b486e3953473b630e3b7bd8a1a
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-07-19 23:37:26 +0200

    create HomeController.php

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return new Response(
+            '<html><body>Wild Series Index</body></html>'
+        );
+    }
+}
