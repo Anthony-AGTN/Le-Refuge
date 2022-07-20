4caa233cfec147470323bbce3238c5978c381d85
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-07-20 17:05:01 +0200

    make CRUD for animal

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,18 +2,81 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
+use App\Form\AnimalType;
+use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/animal', name: 'animal_')]
+#[Route('/animal')]
 class AnimalController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/', name: 'app_animal_index', methods: ['GET'])]
+    public function index(AnimalRepository $animalRepository): Response
     {
         return $this->render('animal/index.html.twig', [
             'website' => 'Le Refuge',
-         ]);
+            'animals' => $animalRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_animal_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AnimalRepository $animalRepository): Response
+    {
+        $animal = new Animal();
+        $form = $this->createForm(AnimalType::class, $animal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $animalRepository->add($animal, true);
+
+            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('animal/new.html.twig', [
+            'website' => 'Le Refuge',
+            'animal' => $animal,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_animal_show', methods: ['GET'])]
+    public function show(Animal $animal): Response
+    {
+        return $this->render('animal/show.html.twig', [
+            'website' => 'Le Refuge',
+            'animal' => $animal,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_animal_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Animal $animal, AnimalRepository $animalRepository): Response
+    {
+        $form = $this->createForm(AnimalType::class, $animal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $animalRepository->add($animal, true);
+
+            return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('animal/edit.html.twig', [
+            'website' => 'Le Refuge',
+            'animal' => $animal,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_animal_delete', methods: ['POST'])]
+    public function delete(Request $request, Animal $animal, AnimalRepository $animalRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $animal->getId(), $request->request->get('_token'))) {
+            $animalRepository->remove($animal, true);
+        }
+
+        return $this->redirectToRoute('app_animal_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/AnimalType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Animal;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnimalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('latinName')
+            ->add('vernacularName')
+            ->add('arrivalDate')
+            ->add('departureDate')
+            ->add('comment')
+            ->add('photo')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Animal::class,
+        ]);
+    }
+}
