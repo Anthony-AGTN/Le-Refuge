e070dd93a5ca40ef1116f7b3f9ad0f28654b2aae
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-07-21 15:09:52 +0200

    set name label for message form add

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,10 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('message')
+            ->add('firstName', TextType::class, ['label' => 'Votre Prénom'])
+            ->add('lastName', TextType::class, ['label' => 'Votre Nom'])
+            ->add('email', EmailType::class, ['label' => 'Votre E-mail'])
+            ->add('message', TextareaType::class, ['label' => 'Ici votre message'])
         ;
     }
 
