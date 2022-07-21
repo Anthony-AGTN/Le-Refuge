a94c4e7b17955364473e9bf3ec84d76bf5ef51ef
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-07-21 14:47:58 +0200

    set form for animal and animalKeeper

--- src/Form/AnimalType.php
+++ src/Form/AnimalType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Animal;
+use App\Entity\AnimalKeeper;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +17,21 @@ class AnimalType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('latinName')
-            ->add('vernacularName')
-            ->add('arrivalDate')
+            ->add('name', TextType::class)
+            ->add('latinName', TextType::class)
+            ->add('vernacularName', TextType::class)
+            ->add('arrivalDate', DateType::class)
             ->add('departureDate')
-            ->add('comment')
-            ->add('photo')
-        ;
+            ->add('comment', TextareaType::class)
+            ->add('photo', FileType::class)
+            ->add('animalKeepers', null, [
+                'class' => AnimalKeeper::class,
+                'multiple' => true,
+                'expanded' => false,
+                'choice_label' => function ($animalKeepers) {
+                    return $animalKeepers->getFirstName() . ' ' . $animalKeepers->getLastName();
+                }
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
