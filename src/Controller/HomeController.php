bcb17b51c322ea346d176ed3b0456a1b5faf0177
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-07-25 00:29:31 +0200

    update header + footer + homepage + homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RefugeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(RefugeRepository $refugeRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'website' => 'Le Refuge',
+            'refuge' => $refugeRepository->findOneBy(['id' => 1]),
+            'website' => 'niotna',
          ]);
     }
 }
