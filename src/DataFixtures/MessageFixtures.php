e8d70036a3243ab15c6c16e1a8e123e7992cd8c1
Anthony Gouton <gouton.anthony@live.fr>
2022-12-31 10:49:35 +0100

    Modification des fixtures des messages

--- src/DataFixtures/MessageFixtures.php
+++ src/DataFixtures/MessageFixtures.php
@@ -14,21 +14,41 @@ class MessageFixtures extends Fixture
         $message->setFirstName('Laurence');
         $message->setLastName('Moresco');
         $message->setEmail('laurence.moresco@test.com');
-        $message->setMessage('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
+        $message->setMessage("Bonjour, Je viens de trouver un animal blessé dans mon jardin.
+        Il s'agit d'un petit écureuil qui a l'air d'avoir été mordu par un chat.
+        Je ne sais pas quoi faire et je me demandais si vous pourriez me donner des conseils
+        ou me dire comment m'en occuper jusqu'à ce que je puisse l'amener chez un vétérinaire.
+        Merci beaucoup de votre aide.
+        Cordialement,
+        Laurence Moresco");
         $manager->persist($message);
 
         $message = new Message();
         $message->setFirstName('Benjamin');
         $message->setLastName('Gomez');
         $message->setEmail('benji.69@test.com');
-        $message->setMessage('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
+        $message->setMessage("Bonjour,
+        Je viens de trouver un petit renard dans mon jardin.
+        Il a l'air d'avoir été blessé par une voiture et il a du mal à bouger.
+        Je ne sais pas quoi faire et je voulais savoir s'il y avait quelque chose que je pouvais faire pour l'aider
+        jusqu'à ce que je puisse l'emmener dans un refuge pour animaux sauvages.
+        Merci beaucoup de votre aide.
+        Cordialement,
+        Benjamin Gomez");
         $manager->persist($message);
 
         $message = new Message();
         $message->setFirstName('Lisa');
         $message->setLastName('Morinton');
         $message->setEmail('morliz@test.com');
-        $message->setMessage('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).');
+        $message->setMessage("Bonjour,
+        Je suis tombé sur un chat errant qui a l'air d'avoir été battu.
+        Il a de nombreuses blessures et il a besoin de soins médicaux urgents.
+        Je ne sais pas quoi faire et je me demandais s'il y avait un moyen de le faire soigner par un vétérinaire
+        ou s'il y a un refuge qui pourrait le prendre en charge.
+        Merci beaucoup de votre aide.
+        Cordialement,
+        Lisa Morinton");
         $manager->persist($message);
 
         $manager->flush();
