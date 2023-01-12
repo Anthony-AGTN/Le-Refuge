e0a21d4b1709693cdf5b467958de4ef62579e5a5
Anthony Gouton <gouton.anthony@live.fr>
2023-01-12 17:43:48 +0100

    Génération d'un CRUD pour l'entité Care

---
+++ src/Controller/CareController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Care;
+use App\Form\CareType;
+use App\Repository\CareRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/care')]
+class CareController extends MainController
+{
+    #[Route('/', name: 'app_care_index', methods: ['GET'])]
+    public function index(CareRepository $careRepository): Response
+    {
+        return $this->render('care/index.html.twig', [
+            'cares' => $careRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_care_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, CareRepository $careRepository): Response
+    {
+        $care = new Care();
+        $form = $this->createForm(CareType::class, $care);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $careRepository->save($care, true);
+
+            return $this->redirectToRoute('app_care_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('care/new.html.twig', [
+            'care' => $care,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_care_show', methods: ['GET'])]
+    public function show(Care $care): Response
+    {
+        return $this->render('care/show.html.twig', [
+            'care' => $care,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_care_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Care $care, CareRepository $careRepository): Response
+    {
+        $form = $this->createForm(CareType::class, $care);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $careRepository->save($care, true);
+
+            return $this->redirectToRoute('app_care_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('care/edit.html.twig', [
+            'care' => $care,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_care_delete', methods: ['POST'])]
+    public function delete(Request $request, Care $care, CareRepository $careRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$care->getId(), $request->request->get('_token'))) {
+            $careRepository->remove($care, true);
+        }
+
+        return $this->redirectToRoute('app_care_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/CareType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Care;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CareType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date')
+            ->add('description')
+            ->add('result')
+            ->add('animal')
+            ->add('animalKeeper')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Care::class,
+        ]);
+    }
+}
