d72fcab456aa2e911e6325289074ef9072098ada
Anthony Gouton <gouton.anthony@live.fr>
2023-01-23 20:18:47 +0100

    Update des fixtures des informations du refuge

--- src/DataFixtures/RefugeFixtures.php
+++ src/DataFixtures/RefugeFixtures.php
@@ -16,6 +16,8 @@ class RefugeFixtures extends Fixture
         $refuge->setPostalCode('69660');
         $refuge->setCity('COLLONGES-AU-MONT-D\'OR');
         $refuge->setCountry('France');
+        $refuge->setPhone('04 84 52 36 57');
+        $refuge->setEmail('contact@lerefuge.com');
         $manager->persist($refuge);
 
         $manager->flush();
